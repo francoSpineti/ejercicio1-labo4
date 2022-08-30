de03417e7aa77f523b9f4bc8462ec95f0b6c44bd
franco <franco.spineti@gmail.com>
2022-08-30 12:12:45 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ejercicio-1';
+
+  promedio : number = 0;
+  suma : number = 0;
+  edadUno : string = "";
+  edadDos : string = "";
+
+  sumar(){
+    this.edadUno = (<HTMLInputElement>document.getElementById("edadUno")).value;
+    this.edadDos = (<HTMLInputElement>document.getElementById("edadDos")).value; 
+    this.suma = (parseInt(this.edadUno) + parseInt(this.edadDos));
+    this.promedio = (this.suma/2);
+  }
+
+  limpiar(){
+    let elementos : any = document.getElementsByName("general");
+    for(let i=0; i < elementos.length; i++){
+        elementos[i].value ="0";
+    }
+  }
+
 }
